3f6596fa5b3af88986bbe6949c94b540bdb6ae6a
Adolligit <adelson.onod@gmail.com>
2023-02-01 03:08:15 -0300

    fix/chore: (menu::analyzer_menu) faltava um espaço em cada linha

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -4,14 +4,14 @@ import sys
 # Requisito 12
 def analyzer_menu():
     """Abrindo PR"""
-    c = 'Selecione uma das opções a seguir:\n'\
-        '0 - Popular o banco com notícias;\n'\
-        '1 - Buscar notícias por título;\n'\
-        '2 - Buscar notícias por data;\n'\
-        '3 - Buscar notícias por tag;\n'\
-        '4 - Buscar notícias por categoria;\n'\
-        '5 - Listar top 5 notícias;\n'\
-        '6 - Listar top 5 categorias;\n'\
+    c = 'Selecione uma das opções a seguir:\n '\
+        '0 - Popular o banco com notícias;\n '\
+        '1 - Buscar notícias por título;\n '\
+        '2 - Buscar notícias por data;\n '\
+        '3 - Buscar notícias por tag;\n '\
+        '4 - Buscar notícias por categoria;\n '\
+        '5 - Listar top 5 notícias;\n '\
+        '6 - Listar top 5 categorias;\n '\
         '7 - Sair.\n'
 
     arr = [
@@ -28,3 +28,6 @@ def analyzer_menu():
         print(arr[int(input(c))-1])
     except (ValueError, IndexError):
         print("Opção inválida", file=sys.stderr)
+
+
+# analyzer_menu()
