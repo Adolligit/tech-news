fab6a0a70e1a317c5732992edfbaee8f5e29f8ab
Adolligit <adelson.onod@gmail.com>
2023-01-31 21:20:07 -0300

    chore: (scraper) adicionando descrições nas funções

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -7,6 +7,7 @@ from tech_news.database import create_news
 
 # Requisito 1
 def fetch(url):
+    """Retorna a estrutura HTML da página"""
     try:
         time.sleep(1)
         response = requests.get(
@@ -23,16 +24,22 @@ def fetch(url):
 
 # Requisito 2
 def scrape_updates(html_content) -> list:
+    """Busca através do HTML por links específicos e os retorna em uma vetor"""
     return Selector(html_content).css(".entry-title a::attr(href)").getall()
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
+    """Busca o botão de ir para a próxima página e retorna sua url"""
     return Selector(text=html_content).css(".next::attr(href)").get()
 
 
 # Requisito 4
 def scrape_news(html_content):
+    """
+        Através do HTML, busca elementos específicos e
+        retorna seus dados estruturados
+    """
     page = Selector(html_content)
     url = page.css('head').xpath('//link[@rel="canonical"]').attrib['href']
     title = page.xpath('//h1[@class="entry-title"]/text()').get().strip()
@@ -61,6 +68,7 @@ def scrape_news(html_content):
 
 # Requisito 5
 def get_tech_news(amount) -> list:
+    """Busca e retorna a quantidade exata de notíticas"""
     url = 'https://blog.betrybe.com'
     news = []
 
